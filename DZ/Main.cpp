52bd6c4945005ea2c48e9b44ae125ccc88d65101
Lev Kosyk <kosyklev@gmail.com>
2023-10-15 10:44:32 +0300

    add function that count all workers above than 60

--- DZ/Main.cpp
+++ DZ/Main.cpp
@@ -2,12 +2,20 @@
 #include"Arbaiter.h"
 using namespace std;
 int main() {
-	Employee* arr = new Employee[5]{
+	int n = 5;
+	Employee* arr = new Employee[n]{
 		{"Pupkin", 2008, "Manager", 12000, "None"},
 		{"Dupkin", 1888, "Sallar", 22000, "PTY"},
 		{"Boyn", 1945, "Counter", 10000, "International universaty"},
 		{"Tylov", 1999, "Manager", 17000, "Kiev universaty"},
 		{"Rubov", 2000, "Manager", 15000, "None"},
-	};
-
+	}; int c = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i].year <= 1963)
+		{
+			c++;
+		}
+	}
+	cout << c;
 }
