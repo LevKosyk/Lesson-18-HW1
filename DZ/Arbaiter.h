2d12d4d33c5434717528fa46f19ce283c4594e35
Lev Kosyk <kosyklev@gmail.com>
2023-10-15 10:34:43 +0300

    add struct Employee

---
+++ DZ/Arbaiter.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+struct Employee
+{
+	char surname[50] = "";
+	int year = 0;
+	char title[50] = "";
+	int sallar = 0;
+	char university[100] = "";
+};
